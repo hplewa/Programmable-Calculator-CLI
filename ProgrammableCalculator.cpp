7e8f06fdb3bdd72199a462c6ea5b7ac2285c16dd
Hubert Plewa <hplewa632@gmail.com>
2018-11-28 12:52:26 -0600

    Reading instructions from file

--- ProgrammableCalculator.cpp
+++ ProgrammableCalculator.cpp
@@ -6,7 +6,11 @@
 using namespace std;
 
 void printRegisters(double* registers) {
-    cout << "w = " << registers[0] << " x = " << registers[1] << " y = " << registers[2] << " z = " << registers[3] << endl;
+    cout << "w = " << registers[0] 
+        << " x = " << registers[1] 
+        << " y = " << registers[2] 
+        << " z = " << registers[3] 
+        << endl;
 }
 
 void printMenu() {
@@ -20,12 +24,42 @@ void printMenu() {
     << "q - quit" << endl;
 }
 
+void printInstructions(vector<string> instructions) {
+    int i = 1;
+    for (auto instruction: instructions) {
+        cout << i++ << ": " << instruction << endl;
+        /*
+        auto id1 = i.at(0);
+        cout << "id1: " << id1 << endl;
+        */
+    }
+}
+
+vector<string> readInstructions(string cmd) {
+    // Read instructions from file
+    vector<string> instructions = vector<string>();
+    string inFileName = cmd.substr(2);
+    ifstream infile (inFileName);
+    string line;
+    if (infile.is_open()) {
+        while (getline(infile, line)) {
+            instructions.push_back(line);
+        }
+        infile.close();
+        cout << "Successfully read " << inFileName << endl;
+    }
+    else {  
+        cout << "Unable to open file " << inFileName << endl;
+    }
+    return instructions;
+}
+
 int main(int argc, char** args) {
     /*
     1. id = id op id.
     2. id = id op constant.
     3. id = constant.
-    4. id ?  go int.
+    4. id ? go int.
     */
     // Instructions read from file
     auto instructions = vector<string>();
@@ -38,24 +72,19 @@ int main(int argc, char** args) {
     for (int i = 0 ; i < 100; i++) {
         getline(cin, cmd);
         // Input
-        if (cmd.find("i") > -1) {
+        if (cmd.find("i") == 0) {
             // Read instructions from file
-            string inFileName = cmd.substr(2);
-            ifstream infile (inFileName);
-            string line;
-            if (infile.is_open()) {
-                while (getline(infile, line)) {
-                    instructions.push_back(line);
-                }
-                infile.close();
-            }
-            else {
-                cout << "Unable to open file " << inFileName << endl;
-            }
+            instructions = readInstructions(cmd);
         }
         // Debug
         else if (cmd.compare("d") == 0) {
-
+            // Pop off the front
+            auto instruction = instructions.front();
+            instructions.erase(instructions.begin());
+            string id1, op1, id2, op2, id3;
+            // Parse instruction
+            id1 = instruction.at(0);
+            op1 = instruction.at(2);
         }
         // Run
         else if (cmd.compare("r") == 0) {
@@ -68,13 +97,7 @@ int main(int argc, char** args) {
         // Print
         else if (cmd.compare("p") == 0) {
             // Print instructions
-            for (auto i: instructions) {
-                cout << i << endl;
-                /*
-                auto id1 = i.at(0);
-                cout << "id1: " << id1 << endl;
-                */
-            }
+            printInstructions(instructions);
         }
         // Help
         else if (cmd.compare("h") == 0) {
